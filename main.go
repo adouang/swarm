9a7bd78d08a96c65b346777173b6c8570c961bc2
adouang <alain.douangpraseuth@gmail.com>
2015-09-23 01:21:21 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/docker/swarm/discovery/kv"
 	_ "github.com/docker/swarm/discovery/nodes"
 	_ "github.com/docker/swarm/discovery/token"
+	_ "github.com/docker/swarm/discovery/serf"
 
 	"github.com/docker/swarm/cli"
 )
